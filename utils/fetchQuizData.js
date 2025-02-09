4de1289bf7413205b5d4e927bcf0230826be3a39
faysal725 <afaysal725@gmail.com>
2025-02-09 17:26:07 +0600

    fetchquizdata changed

--- utils/fetchQuizData.js
+++ utils/fetchQuizData.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const fetchQuizData = async () => {
   try {
     const response = await axios.get(process.env.NEXT_PUBLIC_QUIZ_API);
     return response.data;
   } catch (error) {
-    console.error('Error fetching quiz data:', error);
+    console.error("Error fetching quiz data:", error);
     return null;
   }
 };
